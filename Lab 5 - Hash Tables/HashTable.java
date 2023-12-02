80521fdbc0e9072e216564afd9bbc13aa6de84df
RoopaS4242 <roopas4242@gmail.com>
2023-12-02 12:02:21 -0800

    added few methods to hashtable

--- Lab 5 - Hash Tables/HashTable.java
+++ Lab 5 - Hash Tables/HashTable.java
@@ -1,3 +1,49 @@
 public class HashTable {
+
+    private int capacity, numCollisions;
+    private double loadFactor;
+
+    public HashTable() {
+        capacity = 0;
+    }   
+    public HashTable(int initial) {
+        capacity = initial;
+    }
     
+
+    public int getSize() {
+        return capacity;
+    }
+    public double getLoadFactor() {
+        return loadFactor;
+    }
+    public int getNumCollisions() {
+        return numCollisions;
+    }
+    public Dollar search(Dollar value) {
+
+    }
+    public Dollar getDollarAtIndex(int index) {
+
+    }
+
+    public void remove(Dollar value) {
+        
+    }
+    public void hash (Dollar value) {
+        
+    }
+    public void calculateLoadFactor() {
+        int numItems = 0;
+        for (int i = 0; i < capacity; i++) {
+            if (getDollarAtIndex(i)==null) {
+                numItems++;
+            }
+        }
+        loadFactor = numItems/capacity;
+    }
+
+    public int quadraticProbe(int start, int n) {
+        return ((start + (n*n)) % capacity);
+    }
 }
--- Lab 5 - Hash Tables/lab5main.java
+++ Lab 5 - Hash Tables/lab5main.java
@@ -1,3 +1,5 @@
 public class lab5main {
-    
+    public static void main(String[] args) {
+        HashTable table = new HashTable(29);
+    }
 }
