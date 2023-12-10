bcb38e650739a4304119541098dce71fba10b52d
RoopaS4242 <roopas4242@gmail.com>
2023-12-09 20:45:46 -0800

    Update lab6main.java

--- Lab 6 - Heaps/lab6main.java
+++ Lab 6 - Heaps/lab6main.java
@@ -1,5 +1,32 @@
 public class lab6main {
     public static void main(String[] args) {
+        Queue money = new Queue();
         
+        money.enqueue(new Dollar(57.12));
+        money.enqueue(new Dollar(23.44));
+        money.enqueue(new Dollar(87.43));
+        money.enqueue(new Dollar(68.99));
+        money.enqueue(new Dollar(111.22));
+        money.enqueue(new Dollar(44.55));
+        money.enqueue(new Dollar(77.77));
+        money.enqueue(new Dollar(18.36));
+        money.enqueue(new Dollar(543.21));
+        money.enqueue(new Dollar(20.21));
+        money.enqueue(new Dollar(345.67));
+        money.enqueue(new Dollar(36.18));
+        money.enqueue(new Dollar(48.48));
+        money.enqueue(new Dollar(101.00));
+        money.enqueue(new Dollar(11.00));
+        money.enqueue(new Dollar(21.00));
+        money.enqueue(new Dollar(51.00));
+        money.enqueue(new Dollar(1.00));
+        money.enqueue(new Dollar(251.00));
+        money.enqueue(new Dollar(151.00));
+
+        MinHeap heap = new MinHeap();
+        while (money.countCurrency()>0) {
+            heap.insert(new Dollar(money.dequeue()));
+        }
+
     }
 }
